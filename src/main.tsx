188d7f26f9e445401c5eff8c18db7a7d14e4a97e
Amit Puranik <amitrp2001@gmail.com>
2026-07-22 22:26:09 +0530

    UI changes reverted

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// PrimeReact Core
+// PrimeReact Core + theme (bundled locally — CDN URL was returning 404)
 import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primeicons/primeicons.css";
-
-// 🔥 Theme will be injected dynamically
-const lightTheme = document.createElement("link");
-lightTheme.rel = "stylesheet";
-lightTheme.id = "theme-link";
-lightTheme.href =
-  "https://unpkg.com/primereact/resources/themes/lara-light-indigo/theme.css";
-document.head.appendChild(lightTheme);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
